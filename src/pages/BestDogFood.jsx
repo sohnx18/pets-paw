f3f56225ca31ca8096906263a7d7e1740f8a2d0b
Sohan Atkore <atkoresohan@gmail.com>
2026-01-11 22:10:28 +0530

    Fix Amazon image links and syntax

--- src/pages/BestDogFood.jsx
+++ src/pages/BestDogFood.jsx
@@ -6,21 +6,21 @@ function BestDogFood() {
       id: 1,
       name: "Pedigree Adult Dry Dog Food (3kg)",
       price: "₹679",
-      image: "https://m.media-amazon.com/images/I/81R2m9+uT2L._SL1500_.jpg",
+      image: "https://images-na.ssl-images-amazon.com/images/I/712604bPj2L.jpg",
       buyLink: "https://amzn.to/yourlink1"
     },
     {
       id: 2,
       name: "Drools Adult Dry Dog Food",
       price: "₹722",
-      image: "https://m.media-amazon.com/images/I/71m3zV2k7CL._SL1500_.jpg",
-      buyLink: "https://amzn.to/yourlink2"
+      image: "https://images-na.ssl-images-amazon.com/images/I/71o84Bphv-L.jpg",
+      buyLink: "https://amzn.to/3YT6S6p"
     },
     {
       id: 3,
       name: "Royal Canin Puppy Medium 4kg",
       price: "₹3,776",
-      image: "https://m.media-amazon.com/images/I/614A0idaTvL._SL1200_.jpg",
+      image: "https://images-na.ssl-images-amazon.com/images/I/614A0idaTvL.jpg",
       buyLink: "https://amzn.to/4bz3W66"
     }
   ]
@@ -30,8 +30,8 @@ function BestDogFood() {
       <h1>🐶 Best Dog Food in India (2026)</h1>
 
       <p style={{ maxWidth: "700px", marginTop: "10px", color: "#555" }}>
-        We reviewed and compared the best dog food brands in India based on nutrition,
-        ingredients, price and customer reviews.
+        We reviewed and compared the best dog food brands in India based on
+        nutrition, ingredients, price and customer reviews.
       </p>
 
       <div
@@ -42,7 +42,7 @@ function BestDogFood() {
           flexWrap: "wrap"
         }}
       >
-        {products.map(p => (
+        {products.map((p) => (
           <ProductCard
             key={p.id}
             name={p.name}
