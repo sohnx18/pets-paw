5e4ca00e66c70f8f654ee296c9b30b6afc2d5a72
Sohan Atkore <atkoresohan@gmail.com>
2026-01-11 06:10:37 +0530

    Added Amazon dog products

--- src/pages/Dogs.jsx
+++ src/pages/Dogs.jsx
@@ -1,6 +1,3 @@
-// DEPLOY TEST
-
-
 import ProductCard from "../components/ProductCard"
 
 function Dogs() {
@@ -8,28 +5,28 @@ function Dogs() {
     {
       id: 1,
       name: "Pedigree Adult Dry Dog Food",
-      price: "679",
+      price: 679,
       image: "https://m.media-amazon.com/images/I/712604bPj2L._SX425_.jpg",
       buyLink: "https://amzn.to/49pu56h"
     },
     {
       id: 2,
       name: "Drools Adult Dry Dog Food",
-      price: "₹722",
+      price: 722,
       image: "https://m.media-amazon.com/images/I/71o84Bphv-L._SY355_.jpg",
       buyLink: "https://amzn.to/4jAjgSc"
     },
     {
       id: 3,
       name: "AmazonBasics Dog Rope Toy",
-      price: "₹179 ",
+      price: 179,
       image: "https://m.media-amazon.com/images/I/71kjtvLNy4L._SX425_.jpg",
       buyLink: "https://amzn.to/4qhzJxa"
     },
     {
       id: 4,
       name: "Himalaya Erina-EP Shampoo",
-      price: "₹243",
+      price: 243,
       image: "https://m.media-amazon.com/images/I/51UYPTXkUhL._SY355_.jpg",
       buyLink: "https://amzn.to/4qfROeV"
     }
