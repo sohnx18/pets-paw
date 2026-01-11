15fddc06e626df553319e68b27c37216cdfe26fe
Sohan Atkore <atkoresohan@gmail.com>
2026-01-11 05:36:43 +0530

    test dogs update

--- src/pages/Dogs.jsx
+++ src/pages/Dogs.jsx
@@ -1,3 +1,5 @@
+// TEST: dogs page updated
+
 import ProductCard from "../components/ProductCard"
 
 function Dogs() {
@@ -11,17 +13,24 @@ function Dogs() {
     },
     {
       id: 2,
-      name: "Chew Toy",
-      price: "₹299",
-      image: "https://via.placeholder.com/200",
-      buyLink: "https://www.amazon.in"
+      name: "Drools Adult Dry Dog Food",
+      price: "₹722",
+      image: "https://m.media-amazon.com/images/I/71o84Bphv-L._SY355_.jpg",
+      buyLink: "https://amzn.to/4jAjgSc"
     },
     {
       id: 3,
-      name: "Dog Shampoo",
-      price: "₹199",
-      image: "https://via.placeholder.com/200",
-      buyLink: "https://www.amazon.in"
+      name: "AmazonBasics Dog Rope Toy",
+      price: "₹179 ",
+      image: "https://m.media-amazon.com/images/I/71kjtvLNy4L._SX425_.jpg",
+      buyLink: "https://amzn.to/4qhzJxa"
+    },
+    {
+      id: 4,
+      name: "Himalaya Erina-EP Shampoo",
+      price: "₹243",
+      image: "https://m.media-amazon.com/images/I/51UYPTXkUhL._SY355_.jpg",
+      buyLink: "https://amzn.to/4qfROeV"
     }
   ]
 
