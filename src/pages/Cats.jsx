3308a1a34cdac259b78a2911d5615d0eeca3227f
Sohan Atkore <atkoresohan@gmail.com>
2026-01-11 05:32:17 +0530

    Added Amazon products

--- src/pages/Cats.jsx
+++ src/pages/Cats.jsx
@@ -49,23 +49,23 @@ export default Cats
 const catProducts = [
   {
     id: 1,
-    name: "Cat Food",
-    price: 699,
-    image: "https://via.placeholder.com/200",
-    buyLink: "https://www.amazon.in"
+    name: "Whiskas Adult (1+ Years) Dry Cat Food, Ocean Fish Flavour, 3 kg, Contains 41 Essential Nutrients, Complete & Balanced Nutrition for Adult Cats",
+    price: 990,
+    image: "https://m.media-amazon.com/images/I/81UFCQjgO3L._SX425_.jpg"
+    buyLink: "https://amzn.to/49x1Tgr"
   },
   {
     id: 2,
-    name: "Cat Toy",
+    name: "Foodie Puppies Natural Bentonite Cat Litter Sand",
     price: 249,
-    image: "https://via.placeholder.com/200",
-    buyLink: "https://www.amazon.in"
+    image: "https://m.media-amazon.com/images/I/81lrQ93A10L._SY355_.jpg",
+    buyLink: "https://amzn.to/45Dydgp"
   },
   {
     id: 3,
-    name: "Cat Litter",
-    price: 399,
-    image: "https://via.placeholder.com/200",
-    buyLink: "https://www.amazon.in"
+    name: "Emily Pets Cat Scratcher Cardboard 2 in 1 Reversible Scratching Pad Recyclable Corrugated Scratch Toy with Wave Shape & 10ML Catnip Cat Scratch Lounge for Furniture Protection Scratch Board",
+    price: 281,
+    image: "https://m.media-amazon.com/images/I/714BJl77PGL._SX425_.jpg",
+    buyLink: "https://amzn.to/4sP4yLo"
   }
 ]
