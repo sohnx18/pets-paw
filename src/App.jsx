fd834c28b403a745398a0c96c09bc5a3038313fb
Sohan Atkore <atkoresohan@gmail.com>
2026-01-14 21:48:57 +0530

    Make homepage static for Vercel

--- src/App.jsx
+++ src/App.jsx
@@ -1,32 +1,14 @@
-import { BrowserRouter, Routes, Route } from "react-router-dom";
-
 import Navbar from "./components/Navbar";
 import Footer from "./components/Footer";
-
 import BestDogFood from "./pages/BestDogFood";
-import BestPuppyFood from "./pages/BestPuppyFood";
-import BestCatFood from "./pages/BestCatFood";
-import Dogs from "./pages/Dogs";
-import Cats from "./pages/Cats";
-import NotFound from "./pages/NotFound";
 
 function App() {
   return (
-    <BrowserRouter>
+    <>
       <Navbar />
-
-      <Routes>
-        <Route path="/" element={<BestDogFood />} />
-        <Route path="/best-dog-food" element={<BestDogFood />} />
-        <Route path="/best-puppy-food" element={<BestPuppyFood />} />
-        <Route path="/best-cat-food" element={<BestCatFood />} />
-        <Route path="/dogs" element={<Dogs />} />
-        <Route path="/cats" element={<Cats />} />
-        <Route path="*" element={<NotFound />} />
-      </Routes>
-
+      <BestDogFood />
       <Footer />
-    </BrowserRouter>
+    </>
   );
 }
 
--- vite.config.js
+++ vite.config.js
@@ -3,5 +3,5 @@ import react from "@vitejs/plugin-react";
 
 export default defineConfig({
   plugins: [react()],
-  base: "/",   // 🔴 THIS LINE IS CRITICAL
+  base: "/",
 });
