93ce949ca774c71fea69f9f2e99fab0d2b8bc49f
Sohan Atkore <atkoresohan@gmail.com>
2026-01-14 21:11:29 +0530

    Fix router and deploy ready

--- src/App.jsx
+++ src/App.jsx
@@ -1,49 +1,33 @@
-import { Routes, Route } from "react-router-dom"
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 
-import Navbar from "./components/Navbar"
-import Hero from "./components/Hero"
-import Sections from "./components/Sections"
-import Footer from "./components/Footer"
+import Navbar from "./components/Navbar";
+import Footer from "./components/Footer";
 
-import Dogs from "./pages/Dogs"
-import Cats from "./pages/Cats"
-import BestDogFood from "./pages/BestDogFood"
-import BestPuppyFood from "./pages/BestPuppyFood"
-import BestCatFood from "./pages/BestCatFood"
+import BestDogFood from "./pages/BestDogFood";
+import BestPuppyFood from "./pages/BestPuppyFood";
+import BestCatFood from "./pages/BestCatFood";
+import Dogs from "./pages/Dogs";
+import Cats from "./pages/Cats";
+import NotFound from "./pages/NotFound";
 
 function App() {
   return (
-    <>
+    <BrowserRouter>
       <Navbar />
 
       <Routes>
-        {/* Home */}
-        <Route
-          path="/"
-          element={
-            <>
-              <Hero />
-              <Sections />
-            </>
-          }
-        />
-
-        {/* Store pages */}
-        <Route path="/dogs" element={<Dogs />} />
-        <Route path="/cats" element={<Cats />} />
-
-        {/* SEO Money Pages */}
+        <Route path="/" element={<BestDogFood />} />
         <Route path="/best-dog-food" element={<BestDogFood />} />
         <Route path="/best-puppy-food" element={<BestPuppyFood />} />
         <Route path="/best-cat-food" element={<BestCatFood />} />
-
-        {/* Fallback */}
-        <Route path="*" element={<div style={{ padding: 60, textAlign: 'center' }}><h1>Page not found</h1><p><a href="/">Return home</a></p></div>} />
+        <Route path="/dogs" element={<Dogs />} />
+        <Route path="/cats" element={<Cats />} />
+        <Route path="*" element={<NotFound />} />
       </Routes>
 
       <Footer />
-    </>
-  )
+    </BrowserRouter>
+  );
 }
 
-export default App
+export default App;
